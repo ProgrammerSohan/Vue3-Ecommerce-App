135e6ad55077fcc5af84106de8f8b847ee304a74
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-18 18:31:18 +0600

    1.6_layouts_emitter_opencart_f_t2

--- ecom-vue3/src/components/global/CartDrawer.vue
+++ ecom-vue3/src/components/global/CartDrawer.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
   <div class="drawer">
-    <v-navigation-drawer location="right">
+    <v-navigation-drawer location="right" v-model="drawer">
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem, facilis
         blanditiis iure recusandae voluptatibus optio consequatur vero aliquam ea, porro
@@ -10,3 +10,16 @@
     </v-navigation-drawer>
   </div>
 </template>
+<script>
+export default {
+  inject: ["Emitter"],
+  data: () => ({
+    drawer: false,
+  }),
+  mounted() {
+    this.Emitter.on("openCart", () => {
+      this.drawer = true;
+    });
+  },
+};
+</script>
